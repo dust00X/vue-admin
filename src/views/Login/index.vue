df6ffcc3e4016bd6aea5f24954e17339ab5c444e
Lu <Lu@lu.com>
2020-05-20 12:05:24 +0800

    构建项目、配置

---
+++ src/views/Login/index.vue
@@ -0,0 +1,29 @@
+<!---  --->
+<template>
+    <div>
+      这里是登录页面
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    created(){ 
+
+    },
+    mounted() {
+
+    }
+  }
+</script>
+<style scoped>
+
+</style>
